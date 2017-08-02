5041dcde6a17e09f5f5dbc68db7d75a4e0883784
sunilpatilspp7 <sunilpatilspp7@gmail.com>
2017-08-02 10:57:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/article.one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
+app.get('/ui/sunil.two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'sunil-two.html'));
+});
+
+
 app.get('/ui/image.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'image.jpg'));
 });
