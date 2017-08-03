9319a7245021c5136439383212a6eeea6db14c55
sunilpatilspp7 <sunilpatilspp7@gmail.com>
2017-08-03 22:11:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,67 @@ var articleOne={
 
 };
 
+var articleTwo={
+    title:'Article two|Sunil',
+    heading:'Article two',
+    content:    ` <p>
+               Nature Neuroscience recently published a study with the incredible revelation that brain scanners could predict whether a person would hit a button with their left or right hand a whopping seven seconds before the person was even aware of the decision.
+
+S E V E N. S E C O N D S.
+
+Think about what that means.
+
+That means that the subjects’ deliberating in their mind about which hand to decide to press the button with was pointless.
+
+This means that at least with regard to basic, simple decisions, the subconscious activity of the mind makes its choices before the choice becomes a part of our conscious reality.
+
+If you think about this logically with regards to our emotional reactions to situations, it makes a ton of sense.
+
+Imagine you’re confronted with a stressful situation or one that makes you angry. Your subconscious decides that you are going to be angry and then the thoughts you have literally tell you what to feel and why it is justified. The only thing weird about it is the fact that we genuinely believe it is the other way around!
+
+Most people hear this fact and they think "oh, interesting." It never crosses their mind again - or they might tell it to someone else in passing conversation.
+
+STOP.
+
+Think for a moment how valuable this information is. 
+
+See if you can catch yourself deliberating or justifying some unpleasant emotion. Even when we deliberate, we usually FEEL that there is only one proper course of action to take. If you catch yourself doing it, see if you can catch it again, and again. 
+
+If we make us of this knowledge, it has the potential to change the way we live. 
+             </p>`
+
+};
+
+
+var articleThree={
+    title:'Article three|Sunil',
+    heading:'Article three',
+    content:    ` 
+             <p>
+               Early birds earn better grades in school: In 2008, a study was conducted at Texas University in which people identified themselves as "morning people" or "night owls." It was found that those who identified as early risers, on average, earn a full point more in their GPA than night owls (3.5 GPA vs. 2.5 GPA).
+              </p>
+              <p>
+                    Early risers are more proactive: Christopher Randle, a Harvard psychologist proved that morning people are more likely to identify and set long-term goals for themselves. The study also found that these same early risers are more likely to believe that they're in charge of actually making their goals come to fruition. 
+              </p>
+              <P>
+                        Better at anticipating problems: Morning risers are better at anticipating problems and solving them efficiently and quickly than others. 
+               </P>            
+                  <P>       
+                        Optimism is enhanced: There have been multiple studies conducted that show that morning people more often exhibit traits like optimism, agreeability, satisfaction, and conscientiousness. Although "night owls" often exhibit traits like intelligence and creativity, they're also more likely to be depressed, neurotic and pessimistic. 
+                   </P>    
+                    <p>
+                        Enhanced ability to organize in work and life: Studies have proven that people who wake up early in the morning are better at planning, both in the short term and long term. 
+                    </p>     
+                     <p>      
+                        Take better care of themselves: As logical as it is, there is research behind the fact that morning risers inevitably have more time to exercise and take care of themselves mentally, emotionally and physically before the day starts. The same research conclusively shows that this has the effect of boosting their energy and productivity for the day. 
+                    </p>        
+                     <p>     
+                        Get better sleep: If you can consistently wake up early, you're more likely to be able to go to bed at a reasonable hour. The better sleep is also a product of exerting more energy throughout the day due to a longer day. 
+                     
+             </p>`
+
+};
+
 
 function createTemplate(data){
 var title=data.title;
@@ -66,11 +127,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+  res.send(createTemplate(articleTwo));
 });
 
 app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+  res.send(createTemplate(articleThree));
 });
 
 app.get('/ui/image.jpg', function (req, res) {
