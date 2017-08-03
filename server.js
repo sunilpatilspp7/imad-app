e06be7261695b1e05975753d93a7b61f0c33c239
sunilpatilspp7 <sunilpatilspp7@gmail.com>
2017-08-03 17:50:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var artcileOne={
+    title:'Article One|Sunil',
+    heading:'Article One',
+    Content:    ` <p>
+               Early birds earn better grades in school: In 2008, a study was conducted at Texas University in which people identified themselves as "morning people" or "night owls." It was found that those who identified as early risers, on average, earn a full point more in their GPA than night owls (3.5 GPA vs. 2.5 GPA).
+                        Get better sleep: If you can consistently wake up early, you're more likely to be able to go to bed at a reasonable hour. The better sleep is also a product of exerting more energy throughout the day due to a longer day. 
+             </p>`
+
+};
+
+
+function creatTemplate(data){
+var title=data.title;
+var heading=data.heading;
+var content=data.content;
+
+
+
+
+
+
+
+var htmlTemplate= `
+    <!DOCTYPE html>
+<html>
+    <head>
+         <title>
+      ${title}
+         </title>
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head> 
+     <body>
+         <div class="container">
+         <div>
+             <a href="/">Home</a>
+         </div>
+         <hr>
+         
+         <div>
+            ${heading}
+         </div> 
+         <div>
+             
+              ${content}
+             
+             </div>
+         </div>
+     </body>       
+</html>
+`;
+return htmlTemplate;
+
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,7 +69,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(htmlTemplate(articleOne));
 });
 
 app.get('/ui/article-two', function (req, res) {
