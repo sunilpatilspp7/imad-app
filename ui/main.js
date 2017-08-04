ea6e64cf7a9b5acb287c64f729a306a5a5828b91
sunilpatilspp7 <sunilpatilspp7@gmail.com>
2017-08-04 19:46:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,4 @@ console.log('Loaded!');
 // 
 var element=document.getElementById('main-text');
 
-element.innerHTML="new value";
+element.innerHTML="Hi I am Sunil Patil,This is My first webapp, which is desgined by me.";
