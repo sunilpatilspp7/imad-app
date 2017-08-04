391f015fe78307ea91b49239ae31a7989959dc82
sunilpatilspp7 <sunilpatilspp7@gmail.com>
2017-08-04 19:58:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,13 @@ element.innerHTML="Hi I am Sunil Patil,This is My first webapp, which is desgine
 // move the image
 
 var element=document.getElementById('sunil');
-
+var margineLeft=0;
+function moveRight (){
+    marginLeft=marginLeft+10;
+    sunil.style.marginLeft= marginLeft+'px';
+    
+}
 sunil.onclick=function(){
+    var interval=setInterval(moveRight,100);
     sunil.style.marginLeft='100px';
 };
