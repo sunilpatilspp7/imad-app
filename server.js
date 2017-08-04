2df27775e32bac500afec4e3bc868db02a407d98
sunilpatilspp7 <sunilpatilspp7@gmail.com>
2017-08-04 22:54:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,10 +127,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
  
- var counter=0;
-app.get('/counter',function(req, res){
-    counter=counter+1;
-   res.send(counter.toString()); 
+ var counte=0;
+app.get('/counte',function(req, res){
+    counte=counte+1;
+   res.send(counte.toString()); 
 });
 app.get('/article-one', function (req, res) {
   res.send(createTemplate(articleOne));
