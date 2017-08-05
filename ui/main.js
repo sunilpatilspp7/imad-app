04954ceeef2ab0222902989532508961396b9434
sunilpatilspp7 <sunilpatilspp7@gmail.com>
2017-08-05 11:58:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,1 @@
-console.log('Loaded!');
-// 
-var element=document.getElementById('main-text');
 
-element.innerHTML="Hi I am Sunil Patil,This is My first webapp, which is desgined by me.";
-// move the image
-
-var img=document.getElementById('sunil');
-var marginLeft=0;
-function moveRight () {
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-    
-}
-img.onclick=function() {
-    var interval=setInterval(moveRight, 50);
-
-};
