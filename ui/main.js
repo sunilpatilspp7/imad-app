8799279147970c5e602bb6eb91662c30adb2eb22
sunilpatilspp7 <sunilpatilspp7@gmail.com>
2017-08-05 12:07:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,19 @@ var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick=function(){
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if (request.readyState===XMLHttpRequest.DONE){
+            if (requesr.status === 200 ) {
+                var counter = request.responseText;
+              var span=document.getElementById('count');
+    span.innerHTML = counter.toString();  
+            }
+        }
+    };
+
+    
 };
